77d6eb4f3b0d207bef277918fda3e36899ab39f9
Anto/236 <Antonymbugua1237@gmail.com>
2023-08-23 10:38:08 +0000

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""function determine the fewest number of coins
-   needed to meet a given amount total
+"""
+Change comes from within
 """
 
 
 def makeChange(coins, total):
     """
-    Determine the fewest number of coins needed to meet a given amount total.
-
-    :param coins: List of coin values in your possession
-    :param total: Amount total to be met
-    :return: Fewest number of coins needed to meet total
-             If total is 0 or less, return 0
-             If total cannot be met by any number of coins you have, return -1
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total
     """
     if total <= 0:
         return 0
-    """Initialize a list to store the minimum
-       number of coins needed for each value from 1 to total
-    """
-    min_coins = [float('inf')] * (total + 1)
-    """Base case: 0 coins are needed to make a total of 0"""
-    min_coins[0] = 0
-    """Iterate through each coin value and update the min_coins list"""
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i >= coin:
-                min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
-    """If min_coins[total] is still infinity,
-       it means the total cannot be made using the given coins
-    """
-    if min_coins[total] == float('inf'):
-        return -1
-    return min_coins[total]
+    dp = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
