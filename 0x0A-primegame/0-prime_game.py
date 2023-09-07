ea1a207836acdd68ad88a39f80575da4d23586ba
Anto/236 <Antonymbugua1237@gmail.com>
2023-09-07 11:42:20 +0000

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Prime Game"""
-
-
+"""prime game"""
 def is_prime(n):
     """
     Check if a number is prime.
@@ -25,7 +23,6 @@ def is_prime(n):
         i += 6
     return True
 
-
 def isWinner(x, nums):
     """
     Determine the winner of each game round.
@@ -36,20 +33,23 @@ def isWinner(x, nums):
         the range of numbers for each round.
 
     Returns:
-        str or None: The name of the player that won
-        the most rounds. If the winner cannot be determined, returns None.
+        str or None: The name of the player that won the
+        most rounds. If the winner cannot be determined, returns None.
     """
     if x <= 0 or not nums:
         return None
 
+    def can_win(n):
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        return prime_count % 2 == 1
+
     wins = {"Maria": 0, "Ben": 0}
 
     for n in nums:
-        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-        if prime_count % 2 == 0:
-            wins["Ben"] += 1
-        else:
+        if can_win(n):
             wins["Maria"] += 1
+        else:
+            wins["Ben"] += 1
 
     if wins["Maria"] > wins["Ben"]:
         return "Maria"
